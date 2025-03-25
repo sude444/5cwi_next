563cc8b23f415246b84dae05e21d01476438d4d2
sude <sude.bayar@student.htldornbirn.at>
2025-03-25 19:26:46 +0100

    car array added

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -5,22 +5,61 @@ import { Car } from '@/lib/types/types';
 type Props = {};
 
 
-const car: Car = {
-    "name": "Supercar",
-    "type": "BMW M4",
-    "color": "pink",
-    "motor": {
-        SerialNumber: 4444,
-        hp: 400,
+const cars: Car[] = [
+    {
+        name: "Supercar",
+        type: "BMW M4",
+        color: "pink",
+        motor: {
+            SerialNumber: 4444,
+            hp: 400,
+        },
     },
-}
+    {
+        name: "Roadster",
+        type: "Audi R8",
+        color: "red",
+        motor: {
+            SerialNumber: 1234,
+            hp: 610,
+        },
+    },
+    {
+        name: "Thunder",
+        type: "Ford Mustang",
+        color: "blue",
+        motor: {
+            SerialNumber: 5678,
+            hp: 450,
+        },
+    },
+    {
+        name: "Beast",
+        type: "Chevrolet Camaro",
+        color: "black",
+        motor: {
+            SerialNumber: 9101,
+            hp: 455,
+        },
+    },
+    {
+        name: "Falcon",
+        type: "Mercedes-AMG GT",
+        color: "silver",
+        motor: {
+            SerialNumber: 1122,
+            hp: 523,
+        },
+    },
+];
+
+
 
 export default function page({ }: Props) {
     return (
         <div>
             <h1>Cars</h1>
-            <Card car={car} />
-
+            {cars.map((car: Car) => <Card car={car} />)}
         </div>
     );
 }
--- app/components/ui/cars/Card.tsx
+++ app/components/ui/cars/Card.tsx
@@ -4,5 +4,8 @@ import React from 'react';
 type Props = { car: Car };
 
 export default function Card({ car }: Props) {
-    return <div className='bg-teal-600 border p-4'>Card:{car.name}</div>;
+    return <div className='bg-teal-600 border p-4'>
+        <h2>{car.name}</h2>
+        <div>{car.color}</div>
+    </div>;
 }
